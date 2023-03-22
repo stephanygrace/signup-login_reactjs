603050c4fa242ec6341b9a6d49851436e52cfbe5
stephanygrace <stephanytayong73@gmail.com>
2023-03-22 09:31:08 +0800

    Signup and Signin Design

--- src/Auth.js
+++ src/Auth.js
@@ -1,19 +1,81 @@
 
-import React from "react"
+import React, { useState } from "react"
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function (props) {
+export default function () {
+    
+  let [authMode, setAuthMode] = useState("signin")
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+
+  if (authMode === "signin") {
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+            </p>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div className="Auth-form-container">
       <form className="Auth-form">
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
+          <h3 className="Auth-form-title">Sign Up</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
               type="email"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder="Email Address"
             />
           </div>
           <div className="form-group mt-3">
@@ -21,7 +83,7 @@ export default function (props) {
             <input
               type="password"
               className="form-control mt-1"
-              placeholder="Enter password"
+              placeholder="Password"
             />
           </div>
           <div className="d-grid gap-2 mt-3">
@@ -29,7 +91,8 @@ export default function (props) {
               Submit
             </button>
           </div>
-          <p></p>
+          <p className="text-center mt-2">
+          </p>
         </div>
       </form>
     </div>
