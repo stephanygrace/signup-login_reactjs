d600b1887c0031e2d466fcb9ba8ade992ed5a801
stephanygrace <stephanytayong73@gmail.com>
2023-03-22 10:57:41 +0800

    Connect Front-end to API Login and Signup

--- src/Auth.js
+++ src/Auth.js
@@ -1,81 +1,83 @@
+import React, { useState } from "react";
+import axios from "axios";
 
-import React, { useState } from "react"
+export default function AuthForm() {
+  
+  let [authMode, setAuthMode] = useState("signin");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [name, setName] = useState("");
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default function () {
-    
-  let [authMode, setAuthMode] = useState("signin")
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      let url;
+      let data;
+
+      if (authMode === "signin") {
+        url = "http://localhost/Stelle/php-auth-api/login.php";
+        data = { email, password };
+      } else {
+        url = "http://localhost/Stelle/php-auth-api/register.php";
+        data = { name, email, password };
+      }
+
+      const response = await axios.post(url, data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const changeAuthMode = () => {
-    setAuthMode(authMode === "signin" ? "signup" : "signin")
-  }
-
-  if (authMode === "signin") {
-    return (
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="text-center">
-              Not registered yet?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Sign Up
-              </span>
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <p className="text-center mt-2">
-            </p>
-          </div>
-        </form>
-      </div>
-    )
-  }
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
 
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" onSubmit={handleFormSubmit}>
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign Up</h3>
+          <h3 className="Auth-form-title">
+            {authMode === "signin" ? "Sign In" : "Sign Up"}
+          </h3>
           <div className="text-center">
-            Already registered?{" "}
-            <span className="link-primary" onClick={changeAuthMode}>
-              Sign In
-            </span>
+            {authMode === "signin" ? (
+              <>
+                Not registered yet?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Sign Up
+                </span>
+              </>
+            ) : (
+              <>
+                Already registered?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Sign In
+                </span>
+              </>
+            )}
           </div>
-          <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="e.g Jane Doe"
-            />
-          </div>
+          {authMode === "signup" && (
+            <div className="form-group mt-3">
+              <label>Full Name</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="e.g Jane Doe"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          )}
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
               type="email"
               className="form-control mt-1"
               placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -84,17 +86,17 @@ export default function () {
               type="password"
               className="form-control mt-1"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
             <button type="submit" className="btn btn-primary">
-              Submit
+              {authMode === "signin" ? "Sign In" : "Sign Up"}
             </button>
           </div>
-          <p className="text-center mt-2">
-          </p>
         </div>
       </form>
     </div>
-  )
+  );
 }
